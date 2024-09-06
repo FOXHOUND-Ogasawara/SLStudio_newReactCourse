cfd38c8bb30ef40971410e0758679ab58326a530
FOXHOUND-Ogasawara <124853788+FOXHOUND-Ogasawara@users.noreply.github.com>
2024-09-06 10:38:45 +0900

    HTTP通信修正

--- chapter3/lesson3/http-connect/src/App.tsx
+++ chapter3/lesson3/http-connect/src/App.tsx
@@ -1,15 +1,13 @@
 import "./App.css";
 
 function App() {
-  let users: string[];
   fetch("https://jsonplaceholder.typicode.com/users")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      data.map((user) => {
+      const users: string = data.map((user) => {
         console.log(user.name);
-        users.push(user);
       });
     })
     .catch((error) => console.error("Error fetching data:", error));
@@ -21,11 +19,6 @@ function App() {
         <br />
         HTTP通信の学習
       </h1>
-      <ul>
-        {users.map((user) => {
-          return <li>{user}</li>;
-        })}
-      </ul>
     </>
   );
 }
